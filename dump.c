77ff105ccd693990616d6ab55d2e6a35c6ec7797
Anandbrewery <101007408+Anandbrewery@users.noreply.github.com>
2022-05-05 13:58:37 +0530

    Create dump.c

---
+++ dump.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+struct node
+    {
+        int val;
+        struct node *next;
+    }
+struct node *start=null;
+struct node create_ll( struct node *start)
+{
+    m=newnode=(struct node*)malloc(sizeof)
+}
+int main()
+{
+  start=create_ll(start)
+    
+}
